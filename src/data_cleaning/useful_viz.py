80fc59d0d639b281d75a9a4b071af4ec864db49b
Johnhoy Stephens <jstephens@carthage.edu>
2020-07-08 16:51:40 -0500

    time to reckoning

--- src/data_cleaning/useful_viz.py
+++ src/data_cleaning/useful_viz.py
@@ -12,7 +12,9 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 def show_tree(model,test_or_train,):
+
     """This will plot the decision tree for your designated decision tree model need to fix this later so it does not show the extra text"""
+    from sklearn.tree import plot_tree
     plt.figure(figsize=(25,10))
     p=plot_tree(model,feature_names=test_or_train.columns,filled=True,rounded=True, fontsize=14)
     return p
@@ -28,7 +30,23 @@ def js_drop_unnecessary_feature_columns(df):
               'extraction_type_class', 
              'management_group', 'payment_type', 'quality_group',
              'quantity_group', 'source_type', 'source_class', 
-             'waterpoint_type_group','extraction_type_group',"amount_tsh","management","population"], 
+             'waterpoint_type_group','extraction_type_group',"amount_tsh"], 
+              axis=1, inplace=True)
+    
+    return df
+
+def drop_unnecessary_feature_columns(df):
+    """This function drops all the investigated unnecessary 
+    columns from the features dataframe and returns the 
+    trimmed datadrame.
+    """
+    
+    df.drop(['id', 'date_recorded', 'recorded_by', 'wpt_name',
+             'scheme_name', 'num_private', 'subvillage', 'ward',
+             'amount_tsh', 'extraction_type_class', 'region_code',
+             'management_group', 'payment_type', 'quality_group',
+             'quantity_group', 'source_type', 'source_class', 
+             'waterpoint_type_group', 'installer', 'funder',"extraction_type_group"], 
               axis=1, inplace=True)
     
     return df
